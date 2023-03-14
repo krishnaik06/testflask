103a75678011de8401b4d24604196d375587f8db
Krish Naik <krishnaik06@gmail.com>
2023-03-14 21:19:53 +0530

    First giit commit

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
-from flask import Flask
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings("ignore")
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+app=Flask(__name__)
+
+# import regression and sstandardscaler pickle file
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
+
+
+## Route for homepage
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+## Route for Single data point prediction
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    result=""
+
+    if request.method=='POST':
+
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_data=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data)
+        return render_template('single_prediction.html',result=result[0])
+
+    else:
+        return render_template('home.html')
+
+
+        
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
